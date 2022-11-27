cc8dae58ce2195ca6a2eb2846a75e2e9e8ce9dac
Raknage <toni0haila@gmail.com>
2022-11-27 15:49:34 +0200

    getCpuChoice

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let cpuChoice = () => Math.floor(Math.random() * 3);
-let playerChoice = () => parseInt(prompt("Rock, paper or scissors?", 1));
+function getCpuChoice() {
+  let cpuChoice = null;
+  let x = Math.floor(Math.random() * 3);
+  switch (x) {
+    case 0:
+      cpuChoice = "Rock";
+      break;
+    case 1:
+      cpuChoice = "Paper";
+      break;
+    case 2:
+      cpuChoice = "Scissors";
+      break;
+    default:
+      break;
+  }
+  return cpuChoice;
+}
 
+function getPlayerChoice() {
+  prompt("Rock, paper or scissors?", 1);
+}
