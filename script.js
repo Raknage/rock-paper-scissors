1782a5bf07404ab91f1b9f036be19feba2ac6e89
Raknage <toni0haila@gmail.com>
2022-11-27 16:19:20 +0200

    Result: Tie

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function getPlayerChoice() {
   let playerChoice = prompt("Rock, paper or scissors?", "Rock");
   return playerChoice.trim();
 }
+
+function playRound(cpuChoice = "Rock", playerChoice = "rock") {
+  const cpu = cpuChoice.toLowerCase();
+  const player = playerChoice.toLowerCase();
+  if (cpu === player) {
+    return `Tie! Both played ${cpuChoice}.`;
+  }
+  switch (cpuChoice) {
+    case "Rock":
+      if (playerChoice) {
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+console.log(playRound());
